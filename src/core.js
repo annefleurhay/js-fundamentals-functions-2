4e26e9838bac78974ff4643f996d211426d11575
Annefleur Hay <annefleurhay1@gmail.com>
2023-08-13 17:02:53 +0200

    added assignment functions2 through new pc

--- src/core.js
+++ src/core.js
@@ -1,23 +1,38 @@
 // 1. Create a function named bakingTime that returns 50
-
+function bakingTime() {
+  return 50
+}
+bakingTime()
 // 2. Create a function named remainingBakeTime that accepts one parameter:
 // - the number of minutes your cake has been in the oven.
-//
+
 // It must return how many minutes your cake has left based on your parameter
 // and the result of the bakingTime function
-
+ function remainingBakeTime(oven) {
+  let timeLeft = bakingTime() - oven
+  return timeLeft
+}
+console.log(remainingBakeTime(30))
 // 3. Create a function named calculatePreparationTime that accepts one parameter:
 // - the number of layers your cake has
 //
 // It must return how many minutes it will take to prepare your cake, based on
 // each layer taking 3 minutes to prepare
-
+function calculatePreparationTime(layers) {
+  let totalMinutes = layers * 3
+  return totalMinutes
+}
 // 4. Create a function named totalTimeSpent that accepts two parameters:
 // - the number of layers your cake has
 // - the number of minutes the cake has already been baking in the oven
 // It must return how many minutes in total you have spent making the cake,
 // which is the sum of the preparation time and the number of minutes it's been in the oven.
 // Use your calculatePreparationTime function in the calculation.
+function totalTimeSpent (layers, minutes) {
+const prepTime = calculatePreparationTime(layers)
+const sum = prepTime + minutes
+return sum
+}
 
 // Don't change the code below this line
 module.exports = {
